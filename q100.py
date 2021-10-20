187f28f72c919852a4bbf68e0f291cb7346f41c9
K <83770885+zjxlmq@users.noreply.github.com>
2021-10-20 19:35:57 +0800

    Add files via upload

--- q100.py
+++ q100.py
@@ -1,20 +1,20 @@
 import requests,json
-requests=requests.Session
+u=input("输入你的cookie")
+url='https://comm.ams.game.qq.com/ams/ame/amesvr?ameVersion=0.3&sServiceType=start&iActivityId=407796&sServiceDepartment=group_l&sSDID=febf9f32af4f2d90dedae9cf6d42138c&sMiloTag=AMS-MILO-407796-809290-o1666149353-1634723503687-Suu8i3&_=1634723503691'
 headers={
-    'Host':'api.m.jd.com',
-    'cookie':'pin=jd_64d2983c0c155;wskey=AAJA7Mubf3QZuUT4BJAWk5A-qwrtHYuVC4Nco3PF7up-ce_C92-KD49r02B2W_VWtwU0xqd2GzHFhiMqOxPmg;whwswswws=meQeTpL+aY02ci6jxV1qH+xIFzStf2fdc4KKZtM14bwolvbTTzxchZVuBeaNv0wlhj6XFn7y+ZSWbK4Gn2i+jzuJ4FZzMzm++NB7eOeskDew=;unionwsws={"devicefinger":"eidA02478121f6se5WKkFwYOTPC8kfmzeRkQMuVipZNcF7f7BwLIBsv+CdKShizIbhD0n3Bn6aW2nSgwF7MMTbqeA1YfwyE0Q1h9SZGw\/v+HJ\/lcD\/Bo","jmafinger":"meQeTpL+aY02ci6jxV1qH+xIFzStf2fdc4KKZtM14bwolvbTTzxchZVuBeaNv0wlhj6XFn7y+ZSWbK4Gn2i+jzuJ4FZzMzm++NB7eOeskDew="}',
-    'jdc-backup':'pin=jd_64d2983c0c155;wskey=AAJU4A7Mubf3QZuUT4BJAWk5A-qwrtHYuVC4Nco3PF7up-ce_C92-KD49r02B2W_VWtwU0xqd2GzHFhiMqOxPmg;whwswswws=meQeTpL+aY02ci6jxV1qH+xIFzStf2fdc4KKZtM14bwolvbTTzxchZVuBeaNv0wlhj6XFn7y+ZSWbK4Gn2i+jzuJ4FZzMzm++NB7eOeskDew=;unionwsws={"devicefinger":"eidA02478121f6se5WKkFwYOTPC8kfmzeRkQMuVipZNcF7f7BwLIBsv+CdKShizIbhD0n3Bn6aW2nSgwF7MMTbqeA1YfwyE0Q1h9SZGw\/v+HJ\/lcD\/Bo","jmafinger":"meQeTpL+aY02ci6jxV1qH+xIFzStf2fdc4KKZtM14bwolvbTTzxchZVuBeaNv0wlhj6XFn7y+ZSWbK4Gn2i+jzuJ4FZzMzm++NB7eOeskDew="};',
-    'charset''UTF-8',
-    'accept-encoding':'br,gzip,deflate',
-    'user-agent':'okhttp/3.12.1;jdmall;android;version/10.1.4;build/90060;screen/1080x2340;os/10;network/wifi;'
-
-def login():
-    url='https://api.m.jd.com/client.action?functionId=stationPullServiceV1008&t=1633744690034&appid=MessageCenter&clientVersion=10.1.4&build=90060&client=android&d_brand=vivo&d_model=V1838A&osVersion=10&screen=2340*1080&partner=vivo&oaid=33012dd5e3420876d218673d0d48c2d4fe3be4d2e8690bf30de55aeb745e4017&openudid=dffa96232996e7e5&eid=eidA02478121f6se5WKkFwYOTPC8kfmzeRkQMuVipZNcF7f7BwLIBsv+CdKShizIbhD0n3Bn6aW2nSgwF7MMTbqeA1YfwyE0Q1h9SZGw/v+HJ/lcD/Bo&sdkVersion=29&lang=zh_CN&uuid=dffa96232996e7e5&aid=dffa96232996e7e5&area=22_1930_49324_0&networkType=wifi&wifiBssid=unknown&uts=0f31TVRjBSvkI0yIAfrTGXtK1Nv3Q%2FagmEA1zssgQhviR3Oo1UA%2FtH9Vm93LHGhoSaaAqdiE7c6JORbh8GCtd0BnicmTKKnwN6GbX5TV57gZsV6DDfXEwUem30%2FTjv8Y7xkJSyQPIxnIeHIbPlovvMmWQBrM3pUnDdg7MFO9seSqcU2szGHdy9uTQc%2Bz%2FA7Cok5lHNIDThHoRitdtw2rnw%3D%3D&uemps=0-0&harmonyOs=0&sign=100bd919aa966ef7ab62c917e698db8b0988663177a146cf02ea50e3bc245bd2'
-    data='body=%7B%22pageId%22%3A%22MyJD%22%7D&'
-    k=requests.post(url=url,headers=headers,data=data)
-    l=k.json()
-    print(l)
-login()
-   
-   
-   
+    'Host':'comm.ams.game.qq.com',
+    'content-length':'364',
+    'sec-fetch-mode':'cors',
+    'origin':'https://start.qq.com',
+    'user-agent':'Mozilla/5.0(Linux; Android 10;V1838A) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36',
+    'content-type':'application/x-www-form-urlencoded',
+    'accept':'*/*',
+    'sec-fetch-site':'same-site',
+    'referer':'https://start.qq.com/act/a202110/?viptoken=ce68c5d0604aceff9863363dbce4234d',
+    'accept-encoding':'gzip,deflate,br',
+    'accept-language':'zh-CN,en-US;q=0.9',
+    'cookie':u,
+}
+data='gameId=&sArea=&iSex=&sRoleId=&iGender=&sServiceType=start&objCustomMsg=&areaname=&roleid=&rolelevel=&rolename=&areaid=&iActivityId=407796&iFlowId=809290&g_tk=696630012&e_code=0&g_code=0&eas_url=http%3A%2F%2Fstart.qq.com%2Fact%2Fa202110%2F&eas_refer=http%3A%2F%2Fnoreferrer%2F%3Freqid%3D1beaebd2-86b2-4706-9412-504b801301a1%26version%3D24&sServiceDepartment=group_l'
+k=requests.post(url=url,headers=headers,data=data).text
+print(k)
